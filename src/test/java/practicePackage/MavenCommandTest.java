cb0fde4e4d64af44775093fb5e7520e0fc755038
gunjanup <gunjanprasad429@gmail.com>
2023-11-30 06:54:32 +0530

    second

---
+++ src/test/java/practicePackage/MavenCommandTest.java
@@ -0,0 +1,17 @@
+package practicePackage;
+
+import org.testng.annotations.Test;
+
+public class MavenCommandTest {
+	@Test
+	public void maven()
+	{
+		System.out.println("maven");
+	}
+	@Test
+	public void git()
+	{
+		System.out.println("git");
+	}
+
+}
